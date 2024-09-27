8601a32082e8d7855af1949343e3342d5c69b5fd
Abhishek Tiwari <tiwari.abhishek2021@vitbhopal.ac.in>
2024-09-27 11:38:41 +0530

    Dynamic Routes, Catch All Segments

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,36 +1,10 @@
 import React from "react";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
+import UsersTable from "./UsersTable";
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UsersTable />
     </>
   );
 };
