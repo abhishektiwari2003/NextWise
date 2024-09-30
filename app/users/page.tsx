a361337f86ed4e58627d3271c1b7932d70eea2b1
Abhishek Tiwari <tiwari.abhishek2021@vitbhopal.ac.in>
2024-09-30 14:23:38 +0530

    Example of Programmatic Navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UsersTable from "./UsersTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
@@ -9,6 +10,10 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        {" "}
+        New User{" "}
+      </Link>
       <UsersTable sortOrder={sortOrder} />
     </>
   );
